52d307da07de0f932be2a92869fa89b2c9f0e846
psycho <aali571110@gmail.com>
2025-08-24 18:47:17 +0300

    server .js

---
+++ server.js
@@ -0,0 +1,18 @@
+import dbConnect from "./config/DBConnection.js"
+import dotenv from "dotenv"
+import app from "./app.js"
+
+dotenv.config()
+
+const PORT = process.env.PORT
+
+dbConnect()
+.then((msg)=>{
+    console.log(msg)
+    app.listen(PORT,()=>{
+    console.log("The server is running")
+    })
+})
+.catch(err=>{
+    console.log(err)
+})
